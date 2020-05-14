e0f9adc124082fa618ed9afb307fbcc4a6250cfa
Ben Johnson <bkj.322@gmail.com>
2020-05-14 16:04:16 -0400

    testing

--- run.sh
+++ run.sh
@@ -2,4 +2,89 @@
 
 # run.sh
 
+# --
+# Example problem
+
 python -m simple_tsp.main --inpath data/tsplib/ch150.tsp
+
+# --
+# All EUC_2D TSPLIB problems
+
+python -m simple_tsp.main --inpath data/tsplib/a280.tsp
+python -m simple_tsp.main --inpath data/tsplib/berlin52.tsp
+python -m simple_tsp.main --inpath data/tsplib/bier127.tsp
+# python -m simple_tsp.main --inpath data/tsplib/brd14051.tsp
+python -m simple_tsp.main --inpath data/tsplib/ch130.tsp
+python -m simple_tsp.main --inpath data/tsplib/ch150.tsp
+python -m simple_tsp.main --inpath data/tsplib/d1291.tsp
+# python -m simple_tsp.main --inpath data/tsplib/d15112.tsp
+python -m simple_tsp.main --inpath data/tsplib/d1655.tsp
+# python -m simple_tsp.main --inpath data/tsplib/d18512.tsp
+python -m simple_tsp.main --inpath data/tsplib/d198.tsp
+python -m simple_tsp.main --inpath data/tsplib/d2103.tsp
+python -m simple_tsp.main --inpath data/tsplib/d493.tsp
+python -m simple_tsp.main --inpath data/tsplib/d657.tsp
+python -m simple_tsp.main --inpath data/tsplib/eil101.tsp
+python -m simple_tsp.main --inpath data/tsplib/eil51.tsp
+python -m simple_tsp.main --inpath data/tsplib/eil76.tsp
+python -m simple_tsp.main --inpath data/tsplib/fl1400.tsp
+python -m simple_tsp.main --inpath data/tsplib/fl1577.tsp
+python -m simple_tsp.main --inpath data/tsplib/fl3795.tsp
+python -m simple_tsp.main --inpath data/tsplib/fl417.tsp
+python -m simple_tsp.main --inpath data/tsplib/fnl4461.tsp
+python -m simple_tsp.main --inpath data/tsplib/gil262.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroA100.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroA150.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroA200.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroB100.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroB150.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroB200.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroC100.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroD100.tsp
+python -m simple_tsp.main --inpath data/tsplib/kroE100.tsp
+python -m simple_tsp.main --inpath data/tsplib/lin105.tsp
+python -m simple_tsp.main --inpath data/tsplib/lin318.tsp
+python -m simple_tsp.main --inpath data/tsplib/linhp318.tsp
+python -m simple_tsp.main --inpath data/tsplib/nrw1379.tsp
+python -m simple_tsp.main --inpath data/tsplib/p654.tsp
+python -m simple_tsp.main --inpath data/tsplib/pcb1173.tsp
+python -m simple_tsp.main --inpath data/tsplib/pcb3038.tsp
+python -m simple_tsp.main --inpath data/tsplib/pcb442.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr1002.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr107.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr124.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr136.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr144.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr152.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr226.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr2392.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr264.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr299.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr439.tsp
+python -m simple_tsp.main --inpath data/tsplib/pr76.tsp
+python -m simple_tsp.main --inpath data/tsplib/rat195.tsp
+python -m simple_tsp.main --inpath data/tsplib/rat575.tsp
+python -m simple_tsp.main --inpath data/tsplib/rat783.tsp
+python -m simple_tsp.main --inpath data/tsplib/rat99.tsp
+python -m simple_tsp.main --inpath data/tsplib/rd100.tsp
+python -m simple_tsp.main --inpath data/tsplib/rd400.tsp
+python -m simple_tsp.main --inpath data/tsplib/rl11849.tsp
+python -m simple_tsp.main --inpath data/tsplib/rl1304.tsp
+python -m simple_tsp.main --inpath data/tsplib/rl1323.tsp
+python -m simple_tsp.main --inpath data/tsplib/rl1889.tsp
+python -m simple_tsp.main --inpath data/tsplib/rl5915.tsp
+python -m simple_tsp.main --inpath data/tsplib/rl5934.tsp
+python -m simple_tsp.main --inpath data/tsplib/st70.tsp
+python -m simple_tsp.main --inpath data/tsplib/ts225.tsp
+python -m simple_tsp.main --inpath data/tsplib/tsp225.tsp
+python -m simple_tsp.main --inpath data/tsplib/u1060.tsp
+python -m simple_tsp.main --inpath data/tsplib/u1432.tsp
+python -m simple_tsp.main --inpath data/tsplib/u159.tsp
+python -m simple_tsp.main --inpath data/tsplib/u1817.tsp
+python -m simple_tsp.main --inpath data/tsplib/u2152.tsp
+python -m simple_tsp.main --inpath data/tsplib/u2319.tsp
+python -m simple_tsp.main --inpath data/tsplib/u574.tsp
+python -m simple_tsp.main --inpath data/tsplib/u724.tsp
+python -m simple_tsp.main --inpath data/tsplib/usa13509.tsp
+python -m simple_tsp.main --inpath data/tsplib/vm1084.tsp
+python -m simple_tsp.main --inpath data/tsplib/vm1748.tsp
